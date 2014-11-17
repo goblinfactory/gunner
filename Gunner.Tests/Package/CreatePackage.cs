af11cebc0476df4bd314aeb54cf888b30c57b7cf
Alan Hemmings <goblinfactory@gmail.com>
2014-11-17 14:26:44 +0000

    update the unit test to build the package

---
+++ Gunner.Tests/Package/CreatePackage.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Gunner.Tests.Internal;
+using Ionic.Zip;
+using NUnit.Framework;
+
+namespace Gunner.Tests.Build
+{
+    [TestFixture]
+    public class Packager
+    {
+        // ------------------------------------------------------------------------------------------
+        //                                           PACKAGE SETTINGS
+        // ------------------------------------------------------------------------------------------
+
+        private string[] _packageFileList = new[] {     
+            @"{gunner}\bin\{build}\Gunner.Engine.dll",
+            @"{gunner}\bin\{build}\CommandLine.dll",
+            @"{gunner}\bin\{build}\Gunner.exe",
+            @"{gunner}\bin\{build}\NetMQ.dll",
+            @"{gunner}\bin\{build}\Newtonsoft.Json.dll",
+            @"{gunner}\bin\{build}\Gunner.exe.config",
+            @"{gunner}\bin\{build}\Test404.bat",
+            @"{gunner}\bin\{build}\TestLocal.bat",
+            @"{mechanic}\bin\{build}\mechanic.exe",
+            @"..\..\..\readme.md"
+        };
+
+        string _version =  "Gunner.1.0.zip";
+
+        // ------------------------------------------------------------------------------------------
+        //                                           CREATE PACKAGE 
+        // ------------------------------------------------------------------------------------------
+
+        [Test]
+        public void CreateGunnerPackage()
+        {
+            Test.TraceFeature();
+            GivenAllTheCorrectFilesAreInTheReleaseFolders();
+            WhenICreateAPackage();
+            ThenThePackageIsCreated();
+            AndThePackageShouldBeNamedAccordingToTheVersionNumber();
+            AndThePackageLastModifiedDateTimeShouldBeCurrent();
+        }
+
+
+        // ------------------------------------------------------------------------------------------
+        //                                          STEP DEFINITIONS
+        // ------------------------------------------------------------------------------------------
+
+
+        private List<FileInfo> _packageFIs;
+        private DirectoryInfo _buildOutputPath;
+        private FileInfo _package;
+
+        public void GivenAllTheCorrectFilesAreInTheReleaseFolders()
+        {
+            Test.TraceStep();
+            var mechanic = @"..\..\..\Gunner.Mechanic";
+            var gunner = @"..\..\..\Gunner.Console";
+            var files = _packageFileList.Select(f => 
+                    f.Replace("{build}", Release.CurrentBuild)
+                    .Replace("{mechanic}", mechanic)
+                    .Replace("{gunner}", gunner)
+                ).Select(f => new FileInfo(f)
+            ).ToList();
+            files.ForEach(fi => fi.Exists.Should().BeTrue(fi.FullName + " not exist."));
+            _packageFIs = files;
+        }
+
+        private void WhenICreateAPackage()
+        {
+            Test.TraceStep();
+            _buildOutputPath = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, @"..\..\..\build"));
+            Console.WriteLine( _buildOutputPath.FullName);
+            var packagePath = _buildOutputPath + @"\" + _version;
+            _package = new FileInfo(packagePath);
+            using (ZipFile zip = new ZipFile())
+            {
+                _packageFIs.ForEach(f => zip.AddFile(f.FullName, ""));
+                zip.Save(packagePath);
+            }
+        }
+
+        private void ThenThePackageIsCreated()
+        {
+            Test.TraceStep();
+            _buildOutputPath.GetFiles().Where(f => f.Name.Contains(_version)).Should().NotBeNull();
+        }
+
+        private void AndThePackageShouldBeNamedAccordingToTheVersionNumber()
+        {
+            Test.TraceStep();
+            var files = _buildOutputPath.GetFiles().Where(f => f.Name.EndsWith(".zip"));
+            files.Count().Should().Be(1);
+            var zip = files.ElementAt(0);
+            zip.Name.Should().Be(_version);
+        }
+
+        private void AndThePackageLastModifiedDateTimeShouldBeCurrent()
+        {
+            Test.TraceStep();
+            DateTime.Now.Subtract(_package.LastWriteTime).Should().BeLessOrEqualTo(TimeSpan.FromSeconds(5));
+        }
+
+    }
+}
---
+++ Gunner.Tests/Package/Release.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gunner.Tests.Build
+{
+    public static class Release
+    {
+        public static string CurrentBuild
+        {
+            get
+            {
+#if DEBUG
+                return "DEBUG";
+#else
+                return "RELEASE";
+#endif
+            }
+        }
+    }
+}

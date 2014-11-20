389511d8855f5f5750f2d2106f9f09087f353a2a
Alan Hemmings <goblinfactory@gmail.com>
2014-11-20 12:32:19 +0000

    added build files for easy package delivery

---
+++ build/bin/TestLocal.bat
@@ -0,0 +1,1 @@
+Gunner  -l "mock-counter.log" -g 0 -s 10 -e 100 -r 100 -p 1000 -i 10 -n "latitude" -w "http://localhost:8082/" -u "small1.json,large1.json,small2.json,large2.json,small3.json,large3.json,small4.json,large4.json"
